b8fe8efccf51e2bb0c07087d958296f2485f51e0
Devin Austin <devin.austin@gmail.com>
2008-11-18 03:08:43 +0000

    working on adding photo uploading

--- lib/BoyosPlace/Controller/Photos.pm
+++ lib/BoyosPlace/Controller/Photos.pm
@@ -4,6 +4,7 @@ use strict;
 use warnings;
 use parent 'Catalyst::Controller::HTML::FormFu';
 use IO::File;
+use DateTime;
 
 =head1 NAME
 
@@ -17,7 +18,6 @@ Catalyst Controller.
 
 =cut
 
-
 =head2 index 
   
   display the puppy photos
@@ -25,11 +25,11 @@ Catalyst Controller.
   
 =cut
 
-sub index :Path :Args(0) {
-    my ( $self, $c ) = @_;
+sub index : Path : Args(0) {
+	my ( $self, $c ) = @_;
 
-    $c->stash->{template} = "photos/index.tt2";
-      
+	$c->stash->{template} = "photos/index.tt2";
+
 }
 
 =head2 get_photos
@@ -37,24 +37,25 @@ sub index :Path :Args(0) {
   set up photo stash
   
 =cut
- 
- sub get_photos : Chained('/') PathParth('photo') Args(1) {
- 	my ($self, $c, $photoid) = @_;
- 	
- 	my $photo = $c->model('DB::Photos')->find($photoid);
- 	
- 	if ( $photo == undef ) {
- 		
- 		$c->stash->{error_msg} = "No such photo.";
- 		 		
- 	} else {
- 		
- 		$c->stash->{photo} = $photo;
- 		
- 	}
- 	
- }
- 
+
+sub get_photos : Chained('/') PathParth('photo') Args(1) {
+	my ( $self, $c, $photoid ) = @_;
+
+	my $photo = $c->model('DB::Photos')->find($photoid);
+
+	if ( $photo == undef ) {
+
+		$c->stash->{error_msg} = "No such photo.";
+
+	}
+	else {
+
+		$c->stash->{photo} = $photo;
+
+	}
+
+}
+
 =head2 add_photo
 
   Add a photo to the database
@@ -63,16 +64,28 @@ sub index :Path :Args(0) {
 
 sub add_photo : Path('/photo/add') FormConfig('photos/add.yml') {
 	my ( $self, $c ) = @_;
-	
+
 	$c->stash->{template} = "photos/add.tt2";
 	my $form = $c->stash->{form};
 	if ( $form->submitted_and_valid ) {
-		
-		
-		
+
+		my $photo = $c->model('DB::Photos')->create(
+			{
+				name  => $form->param('photo_name'),
+				path  => {
+					handle   => $c->req->upload('photo')->fh,
+					filename => $c->req->upload('photo')->basename
+				},
+				caption  => $form->param('caption'),
+				uploaded => DateTime->now,
+			}
+		);
+		$c->stash->{entry}      = $photo;
+		$c->stash->{status_msg} = "Successfully uploaded!";
+		$c->detach;
+
 	}
-	
-	
+
 }
 
 =head1 AUTHOR
