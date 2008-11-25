ade22c3df5bb0f3ab4836fc9aa8b2b93f94814ac
Devin Austin <devin.austin@gmail.com>
2008-11-25 23:56:38 +0000

    switched uploads to InflateColumn::FS

--- lib/BoyosPlace/Controller/Photos.pm
+++ lib/BoyosPlace/Controller/Photos.pm
@@ -6,8 +6,8 @@ use parent 'Catalyst::Controller::HTML::FormFu';
 use HTTP::Date;
 use DateTime;
 use Imager;
-use File::Basename;
-use IO::File;
+use MIME::Types;
+use File::MimeInfo ();
 
 __PACKAGE__->mk_accessors(qw(thumbnail_size));
 
@@ -75,6 +75,7 @@ sub add_photo : Path('/photo/add') FormConfig('photos/add.yml') {
 
 	$c->stash->{template} = "photos/add.tt2";
 	my $form = $c->stash->{form};
+	my $mime = MIME::Types->new;
 
      
 	unless ( $c->check_user_roles('admin') ) {
@@ -90,11 +91,11 @@ sub add_photo : Path('/photo/add') FormConfig('photos/add.yml') {
 			{
 				name => $form->param('photo_name'),
 				path => {
-					handle   => $c->req->upload('photo')->fh,
-					filename => $c->req->upload('photo')->basename
+					file   => $c->req->upload('photo')->fh,
 				},
 				caption  => $form->param('caption'),
 				uploaded => DateTime->now,
+				mime     => $mime->mimeTypeOf($c->req->upload('photo')->basename),
 			}
 		);
 
@@ -116,28 +117,28 @@ sub add_photo : Path('/photo/add') FormConfig('photos/add.yml') {
 sub generate_thumbnail : Chained('get_photos') PathPart('thumbnail') Args(0) {
 	my ( $self, $c ) = @_;
     
-    use BoyosPlace;
 	## adapted from CGI::Application::PhotoGallery
 	my $photo = $c->stash->{photo};
-	use Data::Dumper;
-	$c->log->debug(Dumper($photo->path));
-	my $img_path =BoyosPlace->path_to("static","photos", $photo->photoid, $photo->path->filename) . "";
-	$c->log->debug("image path:" . $img_path);
+	
 	my $size    = $self->thumbnail_size;
 	my $cache   = $c->cache;
 	my $key     = $photo->photoid . $photo->path;
-	my $lastmod = ( stat($img_path) )[9];
+	my $mimeinfo = File::MimeInfo->new;
+	#my $lastmod = ( stat($img_path) )[9];
 
 	## here we're setting up cacheing
 	## if there's a key that matches $data
 	## in the cache, we split up the date
 	## and shove it into $reqmod
 	
-	my $fh = IO::File->new($img_path , 'r');
+	my $fh = $photo->path->open('r');
 	
-    $c->log->debug("MiME type:" . (fileparse($photo->path->filename))[2] );
-	$c->res->content_type( (fileparse($photo->path->filename))[2] );
-	$c->res->header( 'Last-Modified' =>HTTP::Date::time2str($lastmod) );
+    $c->log->debug("MiME type:" . $photo->mime);
+	$c->res->content_type( $photo->mime );
+	$c->log->debug("size: " . -s $fh);
+	$c->res->content_length( -s $fh );
+	$c->res->header( "Content-Disposition" => "inline; filename=" . $mimeinfo->extensions($photo->mime) );
+	#$c->res->header( 'Last-Modified' =>HTTP::Date::time2str($lastmod) );
 	binmode $fh;
 	$c->res->body($fh);
 
