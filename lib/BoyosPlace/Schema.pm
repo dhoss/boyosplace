53e6439a7e49b7dff971b109d14b71bd0f4cdca1
Devin Austin <devin.austin@gmail.com>
2008-12-10 14:53:28 +0000

    updated main schema

--- lib/BoyosPlace/Schema.pm
+++ lib/BoyosPlace/Schema.pm
@@ -8,8 +8,8 @@ use base 'DBIx::Class::Schema';
 __PACKAGE__->load_classes;
 
 
-# Created by DBIx::Class::Schema::Loader v0.04005 @ 2008-11-25 13:06:13
-# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:P40B6Dxps6E+2ykKwUtXTg
+# Created by DBIx::Class::Schema::Loader v0.04005 @ 2008-12-01 02:57:02
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:s7ukEW1QxCTLEW7in/J3MQ
 
 
 # You can replace this text with custom content, and it will be preserved on regeneration
