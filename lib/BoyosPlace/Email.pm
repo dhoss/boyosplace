795476c632b0118d451b2ce14977d6e580a88c82
Devin Austin <devin@devin-laptop.(none)>
2009-05-21 12:38:09 -0600

    worked on email test, still not quite working

--- lib/BoyosPlace/Email.pm
+++ lib/BoyosPlace/Email.pm
@@ -3,7 +3,9 @@ package BoyosPlace::Email;
 use Moose;
 use Moose::Util::TypeConstraints;
 use Email::Stuff;
+use Email::Send ();
 use Email::Valid;
+
 
 subtype 'Email', 
     as 'Str', 
@@ -18,10 +20,19 @@ has 'to'      => ( is => 'ro', isa => "Email" );
 has 'from'    => ( is => 'ro', isa => "Email" );
 has 'data'    => ( is => 'ro', isa => "Str" );
 has 'cc'      => ( is => 'ro', isa => "Str" );
+has 'mailer'  => ( is => 'ro', isa => "Str" );
+
+## Borrowed from Reaction::Example::MailerForm
+has '_email_send_object' => (
+  init_arg => 'email_send_object',
+  is => 'ro', isa => 'Email::Send', required => 1
+);
+
 has 'email'   => ( is => 'ro', isa => "Email::Stuff", 
                    default => sub {
                        my $self = shift;
                        Email::Stuff->new(
+                          using         =>$self->_email_send_object,
                           from          =>$self->from,
                           to            =>$self->to,
                           cc            =>$self->cc,
@@ -35,7 +46,7 @@ has 'email'   => ( is => 'ro', isa => "Email::Stuff",
 
 sub send {
     my $self = shift;
-    
+
     $self->email->send or die $!;
     
 }
--- t/email.t
+++ t/email.t
@@ -1,14 +1,17 @@
 use strict;
-use Test::More tests => 1;
+use Test::Most qw[ defer_plan die ];
+use Email::Send::Test;
 use FindBin;
 use lib "$FindBin::Bin/../lib";
 use BoyosPlace::Email;
 
 my $email = BoyosPlace::Email->new(
+        email_send_object => Email::Send->new({ mailer => 'Test' }),
+        mailer  =>  'Test',
         to      => 'dhoss@cpan.org',
         from    => 'devin.austin@gmail.com',
         subject => "testing",
         data    => "testing zee test",
 );
 
-ok( $email->send, "Email sent ok");
+lives_ok{ $email->send }, "Email sent ok";
