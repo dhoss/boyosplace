41a2a4629275634c2fd7fbcc4c56928632442b85
Devin Austin <devin@devin-laptop.(none)>
2009-07-15 21:52:55 -0600

    added youtube model

---
+++ lib/BoyosPlace/Model/YouTube.pm
@@ -0,0 +1,34 @@
+package BoyosPlace::Model::YouTube;
+
+use strict;
+use base 'Catalyst::Model::YouTube';
+
+__PACKAGE__->config(
+    dev_id      => 'AI39si4qdlvPmPLnS6wDk3MsDql3nCS0SG9BibShL9VfBN0qRU09BfCmGw3zASR_Eu6_FR-CsdJnHvgv9Tz26A0oJQNXcqJNlg',
+    options     => {},
+);
+
+=head1 NAME
+
+BoyosPlace::Model::YouTube - YouTube Model Class
+
+=head1 SYNOPSIS
+
+See L<BoyosPlace>
+
+=head1 DESCRIPTION
+
+YouTube Model Class.
+
+=head1 AUTHOR
+
+Devin Austin,,,
+
+=head1 LICENSE
+
+This library is free software, you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
+1;
