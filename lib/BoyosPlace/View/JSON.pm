6094754f06d379f0142c4c5a1f52d7d23d86d1f2
Devin Austin <devin.austin@gmail.com>
2012-05-24 07:17:15 -0700

    added YUI view and HTML view

---
+++ lib/BoyosPlace/View/JSON.pm
@@ -0,0 +1,29 @@
+package BoyosPlace::View::JSON;
+
+use strict;
+use base 'Catalyst::View::JSON';
+
+=head1 NAME
+
+BoyosPlace::View::JSON - Catalyst JSON View
+
+=head1 SYNOPSIS
+
+See L<BoyosPlace>
+
+=head1 DESCRIPTION
+
+Catalyst JSON View.
+
+=head1 AUTHOR
+
+,,,
+
+=head1 LICENSE
+
+This library is free software, you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
+1;
